2982ba624c363113261fc9f72c870b7ee3095d1f
Konrad Bartlett <Konrad.Bartlett@outlook.com>
2021-09-17 01:15:31 -0700

    created a little scroll dog
    dog moves along the side of the screen as you scroll
    transitions to a different image of the dog at the bottom of page

--- src/components/background/background.js
+++ src/components/background/background.js
@@ -1,11 +1,13 @@
 import { useEffect, useRef, useState } from 'react';
 import './background.scss';
+import { Dog } from './dog';
 import { Floater } from './floater';
 import { PawPrint } from './pawprint';
 
 export const Background = () => { 
 
     var pawRefs = useRef({});
+    const dogRef = useRef(null);
 
     const [Floaters, setFloaters] = useState([]);
     const [Paws, setPaws] = useState([]); 
@@ -15,22 +17,38 @@ export const Background = () => {
         setPaws(Array.from(new Array(90)).map((_, index) => <PawPrint key={`paw_${index}`} top={`${(index * (document.documentElement.scrollHeight /100))}px`} side={index % 2 === 0 ? 'left' : 'right'} index={index} ref = {ref => pawRefs.current[index] = ref}/>));
         setFloaters(Array.from(new Array(80)).map((_, index) => <Floater key={`floater_${index}`} index={index} top={`${(index * (document.documentElement.scrollHeight /80))}px`} left={Math.random() * 90 + 5}/>));
 
+        // dogRef.current.children[1].style.opacity = 0;
+        var checkPrints = setInterval(() => {
+            [...Array(90)].forEach((_, index) => {
+                let element = pawRefs.current[index];
+                if( offsetY(element) < offsetY(dogRef.current.children[0]) + 80 ){
+                    element.style.opacity = 0.8;
+                }
+            })
+            if(window.pageYOffset + window.innerHeight > window.document.body.offsetHeight){
+                dogRef.current.children[0].style.opacity = 0;
+                dogRef.current.children[0].style.top = window.pageYOffset + window.innerHeight - 350;
+                dogRef.current.children[1].style.opacity = 1;
+                dogRef.current.children[1].style.top = window.pageYOffset + window.innerHeight - 150;
+                clearInterval(checkPrints)
+            }
+        }, 500);
+
         return(() => {window.removeEventListener('scroll', handleScroll);});
     }, []);
 
     const handleScroll = () => {
-        [...Array(90)].forEach((_, index) => {
-            let element = pawRefs.current[index];
-            if( element.getBoundingClientRect().top < 300 ){
-                element.style.opacity = 0.8;
-            }
-        })
+        if( offsetY(dogRef.current.children[0]) < window.pageYOffset + 300 ){
+            dogRef.current.children[0].style.top = window.pageYOffset + 300;
+            dogRef.current.children[1].style.top = window.pageYOffset + 300;
+        }
     };
 
     return(
         <div id="background">
             <div id="prints">
                 {Paws}
+                <Dog ref={dogRef} />
             </div>
             <div id="floaters">
                 {Floaters}
@@ -41,3 +59,9 @@ export const Background = () => {
     );
 };
     
+
+function offsetY(el) {
+    var rect = el.getBoundingClientRect(),
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return rect.top + scrollTop 
+}
