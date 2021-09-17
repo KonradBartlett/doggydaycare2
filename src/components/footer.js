8fd26858d9c8c790f7ea8cd6a23978cbdd374368
Konrad Bartlett <Konrad.Bartlett@outlook.com>
2021-09-17 01:15:41 -0700

    added link to old site in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,6 +12,7 @@ export const Links = () => {
     return (
         <div>
             <p>Website by: <a className="dark_hover" href="https://konradbartlett.com/" target="_blank" rel="noopener noreferrer nofollow">Konrad Bartlett</a></p>
+            <p><a className="dark_hover" href="https://old.sunshinecoastdog.com/" target="_blank" rel="noopener noreferrer nofollow">Old Version of Site</a></p>
         </div>
     );
 };
